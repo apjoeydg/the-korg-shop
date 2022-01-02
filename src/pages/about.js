a11a476de4ca89b285bf1ccc3ddbf7c31b8e7966
De Greef Joey (s077115) <joey.degreef@student.ap.be>
2022-01-02 14:44:01 +0100

    aanmaak 2 eerste pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <Layout pagetitle="About">
+        <p>About page</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
